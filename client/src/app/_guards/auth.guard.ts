e2d0db69dcceff398e55bc6bb530871b60764672
Александр Десятников <aleksandr.desyatnikov@info-pro.ru>
2021-04-13 23:50:10 +0400

    End of the 6th chapter

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { AccountService } from './../_services/account.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private assountService: AccountService, private toastr: ToastrService) {
+
+  }
+
+  canActivate(): Observable<boolean> {
+    return this.assountService.currentUser$.pipe(map(user => {
+      if (user)
+        return true;
+      this.toastr.error('НЕ прошел');
+    }));
+  }
+
+}
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,9 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import {AccountService} from '../_services/account.service';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -11,21 +13,22 @@ import {AccountService} from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public service: AccountService) { }
+  constructor(public service: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() : void {
     this.service.login(this.model).subscribe(data => {
-      console.log(data);
+      this.router.navigateByUrl('/members');
     }, error => {
-      console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
   logout(): void {
     this.service.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
